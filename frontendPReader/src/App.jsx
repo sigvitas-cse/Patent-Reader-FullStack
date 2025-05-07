6dc2f5cee9a1e081f4bc5910b2af8a423bd08259
SanthoshkumarGudi <santugudi3@gmail.com>
2025-05-07 17:38:43 +0530

    bug resolving

--- frontendPReader/src/App.jsx
+++ frontendPReader/src/App.jsx
@@ -2,22 +2,30 @@ import React from "react";
 import "./App.css";
 import Analysis from "./components/Analysis";
 import Mannual from "./components/Mannual";
-import { BrowserRouter as Router, Route, Link, Routes, BrowserRouter } from 'react-router-dom';
-import { APP_VERSION } from './version';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+  BrowserRouter,
+} from "react-router-dom";
+import { APP_VERSION } from "./version";
 
 function App() {
-   return(
-    <div>
-      <BrowserRouter>
-      <Routes>
-        <Route exact path="/" element={<Analysis />}/>
-        <Route exact path="/mannual" element={<Mannual />}/>
-      </Routes>
-      </BrowserRouter>
+  return (
+    <>
       <div>
-      <footer style={{color:"black"}}>Version: {APP_VERSION}</footer>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Analysis />} />
+            <Route exact path="/mannual" element={<Mannual />} />
+          </Routes>
+        </BrowserRouter>
       </div>
-    </div>
- );
+      <div>
+        <footer style={{ color: "black" }}>Version: {APP_VERSION}</footer>
+      </div>
+    </>
+  );
 }
 export default App;
